1a2d5b0448aaf1fa0c9b928658a203b4ab3d369f
yashbandhiwal <yash.bandhiwal1995@gmail.com>
2024-06-08 20:01:59 +0530

    reshedule everything

---
+++ wire/apis_structure/track.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ * @title : enter mood (create)
+ * @params : enter_mood
+ * @route : POST /api/v1/track/mood
+ * @access : Private
+ * @description : Just enter the mood 
+ * 
+ */
+
+/**
+ * 
+ * @title : delte entry mood (delete)
+ * @params : id
+ * @route : DELETE /api/v1/track/mood
+ * @access : Private
+ * @description : Deletes the mood
+ * 
+ */
+
+/**
+ * 
+ * @title : get list of mood
+ * @params : userID
+ * @route : GET /api/v1/track/mood/getlist
+ * @access : Private
+ * @description : Get list pagination
+ * 
+ */
+
+/**
+ * 
+ * @title : get one of mood
+ * @params : id,userId
+ * @route : GET /api/v1/track/mood/get
+ * @access : Private
+ * @description : Get one mood
+ * 
+ */
+
+/**
+ * 
+ * @title : get one of mood
+ * @params : id,userId
+ * @route : GET /api/v1/track/mood/get
+ * @access : Private
+ * @description : Get one mood
+ * 
+ */
